9855c114ac878015b27bf41c3836903fe021aa3b
Thomas <degris@ualberta.ca>
2012-02-06 09:14:42 +0100

    Demo about scheduling

---
+++ rlpark.example.demos/src/rlpark/example/demos/scheduling/LocalScheduling.java
@@ -0,0 +1,37 @@
+package rlpark.example.demos.scheduling;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import rltoys.experiments.scheduling.interfaces.JobDoneEvent;
+import rltoys.experiments.scheduling.queue.LocalQueue;
+import rltoys.experiments.scheduling.schedulers.LocalScheduler;
+import zephyr.plugin.core.api.signals.Listener;
+
+public class LocalScheduling {
+  static Iterator<? extends Runnable> createJobList() {
+    List<Runnable> jobs = new ArrayList<Runnable>();
+    for (int i = 0; i < 100; i++)
+      jobs.add(new Job());
+    return jobs.iterator();
+  }
+
+  static Listener<JobDoneEvent> createJobDoneListener() {
+    return new Listener<JobDoneEvent>() {
+
+      @Override
+      public void listen(JobDoneEvent eventInfo) {
+        System.out.println("Job done. Result: " + ((Job) eventInfo.done).result());
+      }
+    };
+  }
+
+  public static void main(String[] args) {
+    LocalScheduler scheduler = new LocalScheduler();
+    Iterator<? extends Runnable> jobList = createJobList();
+    Listener<JobDoneEvent> jobDoneListener = createJobDoneListener();
+    ((LocalQueue) scheduler.queue()).add(jobList, jobDoneListener);
+    scheduler.runAll();
+  }
+}
---
+++ rlpark.example.demos/src/rlpark/example/demos/scheduling/ServeurWaitingClients.java
@@ -0,0 +1,18 @@
+package rlpark.example.demos.scheduling;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import rltoys.experiments.scheduling.interfaces.JobDoneEvent;
+import rltoys.experiments.scheduling.network.ServerScheduler;
+import zephyr.plugin.core.api.signals.Listener;
+
+public class ServeurWaitingClients {
+  public static void main(String[] args) throws IOException {
+    ServerScheduler scheduler = new ServerScheduler(ServerScheduler.DefaultPort, 0);
+    Iterator<? extends Runnable> jobList = LocalScheduling.createJobList();
+    Listener<JobDoneEvent> jobDoneListener = LocalScheduling.createJobDoneListener();
+    scheduler.queue().add(jobList, jobDoneListener);
+    scheduler.runAll();
+  }
+}
