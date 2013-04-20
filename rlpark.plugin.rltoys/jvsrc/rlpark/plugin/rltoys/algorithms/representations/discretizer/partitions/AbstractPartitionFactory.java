768913ba75f895577a00533c3caadf23e7aa4ea9
Joseph Modayil <joseph.modayil@gmail.com>
2013-04-20 16:48:06 -0600

    Partitions defined outside the Factories

--- rlpark.plugin.rltoys/jvsrc/rlpark/plugin/rltoys/algorithms/representations/discretizer/partitions/AbstractPartitionFactory.java
+++ rlpark.plugin.rltoys/jvsrc/rlpark/plugin/rltoys/algorithms/representations/discretizer/partitions/AbstractPartitionFactory.java
@@ -2,39 +2,10 @@ package rlpark.plugin.rltoys.algorithms.representations.discretizer.partitions;
 
 import java.util.Random;
 
-import rlpark.plugin.rltoys.algorithms.representations.discretizer.Discretizer;
 import rlpark.plugin.rltoys.algorithms.representations.discretizer.DiscretizerFactory;
 import rlpark.plugin.rltoys.math.ranges.Range;
 
 public abstract class AbstractPartitionFactory implements DiscretizerFactory {
-  public abstract class AbstractPartition implements Discretizer {
-    private static final long serialVersionUID = 5477929434176764517L;
-    public final int resolution;
-    public final double intervalWidth;
-    public final double min;
-    public final double max;
-
-    public AbstractPartition(double min, double max, int resolution) {
-      this.min = min;
-      this.max = max;
-      this.resolution = resolution;
-      intervalWidth = (max - min) / resolution;
-    }
-
-    @Override
-    public String toString() {
-      return String.format("[%f:%f]/%d", min, max, resolution);
-    }
-
-    @Override
-    public int resolution() {
-      return resolution;
-    }
-
-    @Override
-    abstract public int discretize(double input);
-  }
-
   private static final long serialVersionUID = 3356344048646899647L;
   protected final Range[] ranges;
   private double randomShiftRatio = Double.NaN;
@@ -42,6 +13,10 @@ public abstract class AbstractPartitionFactory implements DiscretizerFactory {
 
   public AbstractPartitionFactory(Range... ranges) {
     this.ranges = ranges;
+  }
+
+  public Range[] rangesClone() {
+    return this.ranges.clone();
   }
 
   public void setRandom(Random random, double randomShiftRatio) {
