b0d7d512db8a2b3578e250364303e35696bb1118
Thomas <thomas.degris@inria.fr>
2013-07-09 16:37:00 +0200

    Implement an absorbing in state

--- rlpark.plugin.rltoys/jvsrc/rlpark/plugin/rltoys/agents/rl/LearnerAgentFA.java
+++ rlpark.plugin.rltoys/jvsrc/rlpark/plugin/rltoys/agents/rl/LearnerAgentFA.java
@@ -6,6 +6,7 @@ import rlpark.plugin.rltoys.envio.actions.Action;
 import rlpark.plugin.rltoys.envio.rl.RLAgent;
 import rlpark.plugin.rltoys.envio.rl.TRStep;
 import rlpark.plugin.rltoys.math.vector.RealVector;
+import rlpark.plugin.rltoys.math.vector.implementations.BVector;
 import rlpark.plugin.rltoys.math.vector.implementations.Vectors;
 import zephyr.plugin.core.api.monitoring.annotations.IgnoreMonitor;
 import zephyr.plugin.core.api.monitoring.annotations.Monitor;
@@ -18,15 +19,17 @@ public class LearnerAgentFA implements RLAgent {
   protected double reward;
   @IgnoreMonitor
   protected RealVector x_t;
+  private static BVector absorbingState;
 
   public LearnerAgentFA(ControlLearner control, Projector projector) {
     this.control = control;
     this.projector = projector;
+    absorbingState = new BVector(projector.vectorSize());
   }
 
   @Override
   public Action getAtp1(TRStep step) {
-    RealVector x_tp1 = projector.project(step.o_tp1);
+    RealVector x_tp1 = !step.isEpisodeEnding() ? projector.project(step.o_tp1) : absorbingState;
     reward = step.r_tp1;
     Action a_tp1 = control.step(step.o_t != null ? x_t : null, step.a_t, x_tp1, reward);
     x_t = Vectors.bufferedCopy(x_tp1, x_t);
